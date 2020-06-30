4b96b1455993a279878480718036f46257b94d3b
frankstan33127 <62330183+frankstan33127@users.noreply.github.com>
2020-06-30 17:38:07 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 #Makes the sudo prompt appear so that protonvpn works. Protonvpn doesn't work until it is run as root
 import subprocess
-subprocess.call('pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY /home/$USER/pythongui2/protonvpn_gui.py', shell=True)
+subprocess.call('pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY /home/$USER/ProtonVPN_GUI/protonvpn_gui.py', shell=True)
